390263c7c63b75f6f396fe5eb4e6f4d0763d864a
Thilo Roth <thilo@thilo.io>
2017-08-11 10:50:27 +0800

    'added form validation'

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,15 +6,13 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-    messageSender = '';
-    messageContent = '';
     @Output() messageSend = new EventEmitter<{sender: string, content: string}>();
     constructor() { }
 
     ngOnInit() {
     }
-    onSendMessage() {
-        this.messageSend.emit({sender: this.messageSender, content: this.messageContent});
+    onSendMessage(form: any) {
+        this.messageSend.emit({sender: form.sender, content: form.content});
     }
 
 }
