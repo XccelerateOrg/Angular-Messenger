e01b4e65b4940e126f1e4dbaf59ce33f85faed70
Thilo Roth <thilo@thilo.io>
2017-08-14 18:18:51 +0800

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Message } from './message.model';
-import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,7 @@ import { MessagesService } from './messages.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  messages: Message[] = [ ];
-  term = '';
-  constructor(private messagesService: MessagesService){ }
-
-  ngOnInit() {
-     this.messages = this.messagesService.getMessages()
-  }
-
-  onMessageSent(messageData: {sender: string, content: string}) {
-      this.messages.push(new Message(messageData.content, messageData.sender, new Date()))
-  }
-
+    constructor() {}
+    ngOnInit(){}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,28 @@ import { NgModule } from '@angular/core';
 import { MessagesService } from './messages.service';
 
 import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { MessageComponent } from './message/message.component';
+import { CockpitComponent } from './messenger/cockpit/cockpit.component';
+import { MessageComponent } from './messenger/message/message.component';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './filter.pipe';
+import { MessengerComponent } from './messenger/messenger.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CockpitComponent,
     MessageComponent,
-    FilterPipe
+    FilterPipe,
+    MessengerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [MessagesService],
   bootstrap: [AppComponent]
